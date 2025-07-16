b6efc4c6a706c7d219ebf60f0a130aa2266d536c
BDR14334 <brandonr26.br@gmail.com>
2025-07-16 12:05:36 -0500

    fix supabase error

--- Backend/routes/admin.js
+++ Backend/routes/admin.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 const { createClient } = require('@supabase/supabase-js');
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_KEY = process.env.SUPABASE_SERVICE_KEY;
+const SUPABASE_KEY = process.env.SUPABASE_KEY;
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // Saves images for success stories
